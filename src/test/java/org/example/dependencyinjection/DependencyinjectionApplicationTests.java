4c96cadb5bc27091db152e63bfbae06611bf4ddc
Uwineza Florence <uwinezaflorence20@gmail.com>
2025-05-22 20:11:35 +0200

    spring boot

---
+++ src/test/java/org/example/dependencyinjection/DependencyinjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyinjectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
